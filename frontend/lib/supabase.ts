e6bd8ccd19e01ef5fe2342416f6340cc89a05ad4
jcmartinezb-max <jcmartinezb@secretariajuridica.gov.co>
2026-05-04 12:43:36 -0500

    supa

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -1,8 +1,9 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-// TODO: Reemplaza con tus credenciales de la nueva cuenta de Supabase
-const supabaseUrl = 'https://qttvsaonebquasfaxecs.supabase.co'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0dHZzYW9uZWJxdWFzZmF4ZWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0ODA4MzksImV4cCI6MjA5MzA1NjgzOX0.USV-BSJlABKno2GggdZbS8mNndn-JCJ8-nItmu066WE';
+// Se utilizan variables de entorno con el prefijo EXPO_PUBLIC_ 
+// para que el compilador de Expo las reconozca en la versión web.
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || ''; 
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
